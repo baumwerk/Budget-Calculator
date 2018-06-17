972042b457781478cbc727759331d35ac21721ff
baumwerk <dominik@baumwerk.io>
2018-06-17 19:14:37 +0200

    add updatePercentage function

--- app.js
+++ app.js
@@ -251,6 +251,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Display the budget on the UI
         UICtrl.displayBudget(budget);
     };
+
+    var updatePercentages = function() {
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+    };
     
     
     var ctrlAddItem = function() {
@@ -272,6 +282,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
     };
     
@@ -295,6 +308,9 @@ var controller = (function(budgetCtrl, UICtrl) {
             
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculate and update percentages
+            updatePercentages();
         }
         
     };
